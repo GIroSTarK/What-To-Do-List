3e48276afa9a74ca4b599304ef936e39eb69d711
Illya Katynskyi <katinskyi.illia@lll.kpi.ua>
2023-05-30 19:01:08 +0300

    Add an edit button

--- app.js
+++ app.js
@@ -63,12 +63,14 @@ const createTask = (event) => {
     newTask.classList.add("favorite-task");
     favoriteButton.remove();
     newTask.appendChild(removeFavButton);
+    newTask.appendChild(editButton);
     removeFavButton.addEventListener("click", () => {
       if (newTask.classList.contains("completed")) taskList.prepend(newTask);
       else taskList.appendChild(newTask);
       removeFavButton.remove();
       newTask.classList.remove("favorite-task");
       newTask.appendChild(favoriteButton);
+      newTask.appendChild(editButton);
       checkEmpty(taskList, emptyPar);
       checkEmpty(favoriteList, emptyFavPar);
     });
@@ -76,9 +78,35 @@ const createTask = (event) => {
     checkEmpty(favoriteList, emptyFavPar);
   });
 
+  const editButton = document.createElement("button");
+  editButton.classList.add("btn", "edit-btn");
+  removeFavButton.setAttribute("title", "Редагувати");
+  editButton.addEventListener("click", () => {
+    const termInput = document.createElement("input");
+    termInput.classList.add("term-input");
+    termInput.value = newTask.textContent;
+    const termButton = document.createElement("button");
+    termButton.classList.add("main-button", "term-button");
+    termButton.textContent = "Змінити";
+    termButton.addEventListener("click", () => {
+      if (termInput.value !== "") {
+        newTask.textContent = termInput.value;
+        newTask.appendChild(deleteButton);
+        newTask.appendChild(completeButton);
+        if (taskList.contains(newTask)) newTask.appendChild(favoriteButton);
+        else newTask.appendChild(removeFavButton);
+        newTask.appendChild(editButton);
+      }
+    });
+    newTask.appendChild(termInput);
+    termInput.focus();
+    newTask.appendChild(termButton);
+  });
+
   newTask.appendChild(deleteButton);
   newTask.appendChild(completeButton);
   newTask.appendChild(favoriteButton);
+  newTask.appendChild(editButton);
   taskList.appendChild(newTask);
   inputField.value = "";
   checkEmpty(taskList, emptyPar);
